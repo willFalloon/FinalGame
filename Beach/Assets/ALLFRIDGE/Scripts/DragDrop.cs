9000934a070dd5f5c4b369e155d069e744142558
willFalloon <20091@wc.school.nz>
2024-09-03 00:25:59 +1200

    switched FridgeScene to BEach and added end of level

---
+++ Beach/Assets/ALLFRIDGE/Scripts/DragDrop.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class DragDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler
+{
+    [SerializeField] private Canvas canvas;
+
+    private RectTransform rectTransform;
+    private CanvasGroup canvasGroup;
+    private CanvasScaler canvasScaler;
+
+    private void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        canvasGroup = GetComponent<CanvasGroup>();
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnBeginDrag");
+        canvasGroup.alpha = .6f;
+        canvasGroup.blocksRaycasts = false;
+
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        
+
+        Debug.Log("OnDrag");
+        rectTransform.anchoredPosition += eventData.delta / (canvasScaler.scaleFactor * 1.6f);
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnEndDrag");
+        GameObject dropTarget = eventData.pointerCurrentRaycast.gameObject;
+        canvasGroup.alpha = 1f;
+        canvasGroup.blocksRaycasts = true;
+
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log("OnPointerDown");
+
+
+    }
+
+}
---
+++ Beach/Assets/ALLFRIDGE/Scripts/EatDrop.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class EatDrop : MonoBehaviour, IDropHandler
+{
+    public GameObject twoX;
+    Quaternion randomRotation = Random.rotation;
+
+    public float onScreenTime = 2.0f;
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        Debug.Log("OnDrop");
+
+        if (eventData.pointerDrag != null)
+        {
+
+            eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = GetComponent<RectTransform>().anchoredPosition;
+            throw new System.NotImplementedException();
+
+        }
+        if (twoX != null)
+        {
+            Vector3 randomPosition = new Vector3(
+            transform.position.x + Random.Range(-600f, -100f),
+            transform.position.y + Random.Range(-300f, 100f));
+
+
+
+            GameObject spawnedtwoX = Instantiate(twoX, randomPosition, Random.rotation);
+
+            float randomScale = Random.Range(0.2f, 0.7f);
+            spawnedtwoX.transform.SetParent(transform, false);
+            spawnedtwoX.transform.localScale = new Vector3(randomScale, randomScale, randomScale);
+
+        }
+
+    }
+
+
+
+
+
+
+
+
+}
